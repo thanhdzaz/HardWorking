a8788397c2018e1b5d8130cd01dbf0b20c4c4e86
Cầm Văn Thành <camvanthnhaz@gmail.com>
2022-06-26 01:29:28 +0700

    fix build fail

--- craco.config.js
+++ craco.config.js
@@ -62,6 +62,11 @@ module.exports = {
             ])
       );
 
+      const instanceOfMiniCssExtractPlugin = webpackConfig.plugins.find(
+        (plugin) => plugin.options && plugin.options.ignoreOrder != null,
+      );
+      instanceOfMiniCssExtractPlugin.options.ignoreOrder = true;
+      
       return webpackConfig;
     },
   },
